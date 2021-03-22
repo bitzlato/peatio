a6e9c89401c4a3b15fc9bede49b6eabc3529d51e
Danil Pismenny <danil@brandymint.ru>
2021-03-22 09:14:46 +0300

    Minor fix in deposits polling

--- app/jobs/cron/deposits_polling.rb
+++ app/jobs/cron/deposits_polling.rb
@@ -5,10 +5,9 @@ module Jobs
     module DepositsPolling
       def self.process
         Wallet.active.find_each do |w|
+          ws = WalletService .new(w)
           next unless ws.support_polling?
-          WalletService
-            .new(w)
-            .poll_transfers!
+          ws.poll_transfers!
         rescue StandardError => e
           report_exception_to_screen(e)
         end
