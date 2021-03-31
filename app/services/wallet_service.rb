3a55cf8fc2fd3bf1ef1e2ea92af502cc91708610
Danil Pismenny <danil@brandymint.ru>
2021-03-31 10:10:12 +0300

    fix specs

--- app/services/wallet_service.rb
+++ app/services/wallet_service.rb
@@ -113,7 +113,7 @@ class WalletService
     withdrawal.with_lock do
       save_transaction(transaction.as_json.merge(from_address: @wallet.address), withdrawal)
       withdrawal.update metadata: withdrawal.metadata.merge( 'links' => transaction.options['links'] ) if transaction.options&.has_key? 'links'
-      withdraw.success! if withdraw.confirming? && transaction.options&.fetch('completed', false)
+      withdrawal.success! if withdrawal.confirming? && transaction.options&.fetch('completed', false)
     end if transaction.present?
 
     transaction
