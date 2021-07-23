88ab532b01f2bb37094cce85556d185bf5268532
Danil Pismenny <danil@brandymint.ru>
2021-07-23 15:41:56 +0300

    Disable sentry for staging

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,5 +4,5 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.traces_sample_rate = 0.01
   config.send_default_pii = true
-  config.enabled_environments = %w[production staging]
+  config.enabled_environments = %w[production]
 end
