14db27ef4abffb2994989008c59eeb0820aa4ac6
Danil Pismenny <danil@brandymint.ru>
2021-08-10 15:42:43 +0300

    Fix jobs/cron/payment_address_balances

--- app/jobs/cron/payment_address_balancer.rb
+++ app/jobs/cron/payment_address_balancer.rb
@@ -2,17 +2,20 @@ module Jobs
   module Cron
     class PaymentAddressBalancer
       def self.process
-        PaymentAddress.find_each do |payment_address|
-          update_balance pa
-        end
+        PaymentAddress.find_each(&method(:update_balance))
         sleep 10
       end
 
       def self.update_balance payment_address
         Sentry.with_scope do |scope|
           scope.set_tags(payment_address_id: payment_address.id)
-          update!(
-            balances: payment_address.wallet.blockchain.blockchain_api.load_balance!(payment_address.address, payment_address.currency),
+          balances = payment_address.
+            wallet.
+            blockchain.
+            blockchain_api.
+            load_balance!(payment_address.address, payment_address.currency.id)
+          payment_address.update!(
+            balances: balances,
             balances_updated_at: Time.zone.now
           )
         rescue StandardError => err
