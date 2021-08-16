f790c0328ad2e87e4914e3fe5f78ec05ba53a8de
Danil Pismenny <danil@brandymint.ru>
2021-08-16 08:26:53 +0300

    deposit_funds_locked true by default

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,6 +3,6 @@
 require 'peatio/app'
 
 Peatio::App.define do |config|
-  config.set(:deposit_funds_locked, 'false', type: :bool)
+  config.set(:deposit_funds_locked, 'true', type: :bool)
   config.set(:manual_deposit_approval, 'false', type: :bool)
 end
