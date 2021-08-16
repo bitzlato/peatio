20daa72815ef85a6a8cf253a967e433cf31dc552
Artem Belankov <temacxz@gmail.com>
2021-08-16 17:01:58 +0500

    Refactor order_services/create_order, move compute_locked Order->service

--- app/services/order_services/create_order.rb
+++ app/services/order_services/create_order.rb
@@ -19,10 +19,9 @@ module OrderServices
 
     private
 
-    def build_order(market:,side:, ord_type:, price:, volume:)
-      order_subclass = side == 'sell' ? OrderAsk : OrderBid
+    def build_order(market: ,side:, ord_type:, price:, volume:)
       uuid = UUID.generate
-
+      member_account = get_member_account(side: side, market: market)
       # Single Order can produce multiple Trades
       # with different fee types (maker and taker).
       # Since we can't predict fee types on order creation step and
@@ -36,7 +35,32 @@ module OrderServices
       maker_fee = trading_fee.maker
       taker_fee = trading_fee.taker
 
-      order = order_subclass.new(
+      member_account.with_lock do
+        if side == 'sell'
+          order_subclass = OrderAsk,
+          locked_value = calc_sell_compute_locked(
+            ord_type: ord_type,
+            volume: volume,
+            market: market,
+          )
+        else
+          order_subclass = OrderBid
+          locked_value = calc_buy_compute_locked(
+            ord_type: ord_type,
+            price: price,
+            volume: volume,
+            market: market,
+            member_balance: member_account.balance,
+          )
+        end
+      end
+
+      raise(
+        ::Account::AccountError,
+        "member_balance > locked = #{member_account.balance} > #{locked_value}"
+      ) if member_account.balance < locked_value
+
+      order_subclass.create!(
         state:         ::Order::PENDING,
         member:        @member,
         ask:           market.base_unit,
@@ -52,30 +76,41 @@ module OrderServices
         uuid:          uuid,
         maker_fee:     maker_fee,
         taker_fee:     taker_fee,
+        locked:        locked_value,
+        origin_locked: locked_value,
       )
+    end
 
-      locked_value = if order.ord_type == 'market' && order.side == 'buy'
-        [
-          order.compute_locked * OrderBid::LOCKING_BUFFER_FACTOR,
-          order.member_balance
-        ].min
-      else
-        order.compute_locked
+    def calc_sell_compute_locked(ord_type:, volume:, market:)
+      case ord_type
+      when 'limit'
+        volume
+      when 'market'
+        estimate_required_funds(OrderBid.get_depth(market.id)) { |_, value| v }
       end
+    end
 
-      order.assign_attributes(
-        locked: locked_value,
-        origin_locked: locked_value
-      )
+    def calc_buy_compute_locked(ord_type:, price:, volume:, market:, member_balance:)
+      value = case ord_type
+              when 'limit'
+                price * volume
+              when 'market'
+                funds = estimate_required_funds(
+                  OrderAsk.get_depth(market.id)
+                ) { |price, volume| price * volume }
+                # Maximum funds precision defined in Market::FUNDS_PRECISION.
+                funds.round(Market::FUNDS_PRECISION, BigDecimal::ROUND_UP)
+              end
+      [
+        value * OrderBid::LOCKING_BUFFER_FACTOR,
+        member_balance
+      ].min
+    end
 
-      order.save!
-
-      raise(
-        ::Account::AccountError,
-        "member_balance > locked = #{order.member_balance} > #{order.locked}"
-      ) if order.member_balance < order.locked
-
-      order
+    def get_member_account(side:, market:)
+      currency_unit = side == 'sell' ? market.base_unit : market.quote_unit
+      currency = Currency.find(currency_unit)
+      @member.get_account(currency)
     end
 
     def submit_order!(order)
