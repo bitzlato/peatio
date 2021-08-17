83a2c33b82cdc1d2fd6100dcb54d7301136363a2
Danil Pismenny <danil@brandymint.ru>
2021-08-17 17:37:15 +0300

    Minor refactor GasRefueler

--- app/gateways/ethereum_gateway/gas_refueler.rb
+++ app/gateways/ethereum_gateway/gas_refueler.rb
@@ -5,7 +5,6 @@ class EthereumGateway
     REFUEL_GAS_FACTOR = Settings.ethereum.refuel_gas_factor
 
     Error = Class.new StandardError
-    NoReason = Class.new Error
     NoTokens = Class.new Error
     Balanced = Class.new Error
 
@@ -13,38 +12,27 @@ class EthereumGateway
       ethereum_balance = load_basic_balance target_address
       raise "ethereum_balance #{ethereum_balance} must be an Integer" unless ethereum_balance.is_a? Integer
 
-      raise NoTokens if tokens_count.zero?
+      if tokens_count.zero?
+        logger.info("No tokens on address #{target_address}")
+        raise NoTokens
+      end
 
       gas_limit = TransactionCreator::DEFAULT_ETH_GAS_LIMIT
       gas_price ||= (fetch_gas_price * REFUEL_GAS_FACTOR).to_i
 
-      required_amount =  tokens_count * TransactionCreator::DEFAULT_ERC20_GAS_LIMIT * gas_price
-
-      transaction_amount = required_amount - ethereum_balance
-
-      raise Balanced if ethereum_balance > required_amount
-
-      #if ethereum_balance < required_amount
-        #logger.info("No reason to create gas refueling eth transaction #{gas_wallet_address} -> #{target_address}"\
-                    #" ethereum_balance: #{ethereum_balance}, tokens_count: #{tokens_count},"\
-                    #" required_amount: #{required_amount} gas_price:#{gas_price} gas_limit:#{gas_limit}"\
-                    #" transaction amount: #{transaction_amount}"\
-                    #"ethereum_balance(#{ethereum_balance} < required_amount(#{required_amount})")
-        #raise AmountError, 'Current balance less then required'
-      #end
+      transaction_amount = tokens_count * TransactionCreator::DEFAULT_ERC20_GAS_LIMIT * gas_price - ethereum_balance
 
       if transaction_amount.positive?
         logger.info("Create gas refueling eth transaction #{gas_wallet_address} -> #{target_address}"\
                     " ethereum_balance: #{ethereum_balance}, tokens_count: #{tokens_count},"\
-                    " required_amount: #{required_amount} gas_price:#{gas_price} gas_limit:#{gas_limit}"\
+                    " gas_price:#{gas_price} gas_limit:#{gas_limit}"\
                     " transaction amount: #{transaction_amount}")
       else
-        logger.info("Skip creating gas refueling eth transaction (current amount is enough) #{gas_wallet_address} -> #{target_address}"\
+        logger.info("No reason to create gas refueling eth transaction #{gas_wallet_address} -> #{target_address}"\
                     " ethereum_balance: #{ethereum_balance}, tokens_count: #{tokens_count},"\
-                    " required_amount: #{required_amount} gas_price:#{gas_price} gas_limit:#{gas_limit}"\
+                    " gas_price:#{gas_price} gas_limit:#{gas_limit}"\
                     " transaction amount: #{transaction_amount}")
-        raise AmountError, 'Current balance less then required'
-        return nil
+        raise Balanced
       end
 
       tx = TransactionCreator.new(client).create_eth_transaction!(
