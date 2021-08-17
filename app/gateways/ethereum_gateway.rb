854212fd66a28d97c51e08145dec62d9dd1a8f42
Danil Pismenny <danil@brandymint.ru>
2021-08-17 13:11:28 +0300

    Fix EthereumGateway.create_transaction!

--- app/gateways/ethereum_gateway.rb
+++ app/gateways/ethereum_gateway.rb
@@ -60,7 +60,8 @@ class EthereumGateway < AbstractGateway
 
 
     raise 'amount must be a Money' unless amount.is_a? Money
-    t = TransactionCreator
+    hash_to_transaction(
+      TransactionCreator
       .new(client)
       .call(from_address: from_address,
             to_address: to_address,
@@ -68,10 +69,7 @@ class EthereumGateway < AbstractGateway
             secret: secret,
             contract_address: contract_address,
             subtract_fee: subtract_fee)
-
-    return unless t
-    t.amount =  amount.currency.to_money_from_units(t.amount)
-    t
+    )
   end
 
   def collect_deposit!(deposit)
@@ -129,6 +127,8 @@ class EthereumGateway < AbstractGateway
   private
 
   def hash_to_transaction(hash)
+    return if hash.nil?
+    hash = hash.as_json if hash.is_a? Peatio::Transaction
     currency = blockchain.find_money_currency(hash.fetch(:contract_address))
     Peatio::Transaction.new hash.merge(currency_id: currency.id, amount: currency.to_money_from_units(hash.fetch(:amount)))
   end
