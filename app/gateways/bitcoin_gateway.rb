0e92178edb50142a74e0528f695b0be4387b8ab7
Danil Pismenny <danil@brandymint.ru>
2021-08-18 09:35:42 +0300

    Fix specs

--- app/gateways/bitcoin_gateway.rb
+++ app/gateways/bitcoin_gateway.rb
@@ -3,6 +3,14 @@ class BitcoinGateway < AbstractGateway
     true
   end
 
+  def self.valid_address?(address)
+    AdequateCryptoAddress.valid? address, :btc
+  end
+
+  def self.normalize_address(address)
+    AdequateCryptoAddress.address(address, :btc).address
+  end
+
   private
 
   def build_client
