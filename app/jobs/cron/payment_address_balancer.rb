19e8a9c6f8546ce8a0b2d9a046c4bb76bb3ae48d
Danil Pismenny <danil@brandymint.ru>
2021-08-18 14:11:11 +0300

    Fix PaymentAddressBalancer

--- app/jobs/cron/payment_address_balancer.rb
+++ app/jobs/cron/payment_address_balancer.rb
@@ -6,17 +6,10 @@ module Jobs
         sleep 10
       end
 
-      def self.update_balance(payment_address)
-        if payment_address.blockchain.gateway.enable_personal_address_balance?
-          update_balance_by_currency payment_address
-        else
-          update_balances(payment_address)
-        end
-      end
-
       def self.update_balances payment_address
+        return unless payment_address.blockchain.gateway.enable_personal_address_balance?
         payment_address.update!(
-          balances: payment_address.blockchain.gateway.load_balances(payment_address.address).select { |c,v| v.positive? }.transform_keys { |k| k.id },
+          balances: convert_balances(payment_address.blockchain.load_balances(payment_address.address)),
           balances_updated_at: Time.zone.now
         )
       rescue StandardError => err
@@ -24,21 +17,11 @@ module Jobs
         report_exception err, true, payment_address_id: payment_address.id
       end
 
-      def self.update_balance_by_currency payment_address
-        balances = payment_address.currencies.each_with_object({}).each do |currency, hash|
-          hash[currency.id.downcase] =
-            payment_address.
-            blockchain.
-            gateway.
-            load_balance(payment_address.address, currency)
+      def self.convert_balances(balances)
+        balances.each_with_object({}) do |(k,v), a|
+          currency_id = k.is_a?(Money::Currency) ? k.id.downcase : k
+          a[currency_id] = v.to_d
         end
-        payment_address.update!(
-          balances: balances,
-          balances_updated_at: Time.zone.now
-        )
-      rescue StandardError => err
-        Rails.logger.warn "#{err} for payment_address id #{payment_address.id}"
-        report_exception err, true, payment_address_id: payment_address.id
       end
     end
   end
