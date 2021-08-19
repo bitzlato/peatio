333a4d46357da1a22c1e774f57129490e9ec4c77
Danil Pismenny <danil@brandymint.ru>
2021-08-19 12:41:36 +0300

    Use local redis by default

--- config/environments/shared/cache.rb
+++ config/environments/shared/cache.rb
@@ -5,6 +5,6 @@ Rails.application.configure do
   if ENV.true?("REDIS_CLUSTER")
     config.cache_store = :redis_cache_store, { driver: :hiredis, cluster: [ENV.fetch('REDIS_URL')], password: ENV.fetch('REDIS_PASSWORD') }
   else
-    config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL') }
+    config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
   end
 end
