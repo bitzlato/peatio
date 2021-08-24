b5283533f7e1758620e69cc1cc3e002a845def34
Artem Belankov <temacxz@gmail.com>
2021-08-24 11:57:13 +0500

    Remove legacy account factory code

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,18 +1,3 @@
-# # encoding: UTF-8
-# # frozen_string_literal: true
-
-# module AccountFactory
-#   def create_account(*arguments)
-#     currency   = Symbol === arguments.first ? arguments.first : :usd
-#     attributes = arguments.extract_options!
-#     attributes.delete(:member) { create(:member) }.get_account(currency).tap do |account|
-#       account.update!(attributes)
-#     end
-#   end
-# end
-
-# RSpec.configure { |config| config.include AccountFactory }
-
 # encoding: UTF-8
 # frozen_string_literal: true
 
