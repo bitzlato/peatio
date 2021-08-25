8d7f2cc6bd6464a37ac8f7bbe1d25b8d2065da4b
Danil Pismenny <danil@brandymint.ru>
2021-08-25 15:42:46 +0300

    Don't notify when transcation has no reference and failed

---
+++ app/models/concerns/transaction_references.rb
@@ -0,0 +1,25 @@
+module TransactionReferences
+  private
+
+  def find_reference
+    find_withdraw_as_reference || find_deposit_as_reference || find_wallet_as_reference
+  end
+
+  def find_withdraw_as_reference
+    blockchain.withdraws.find_by_txid(txid) if txid.present?
+  end
+
+  def find_deposit_as_reference
+    blockchain.deposits.find_by(txid: txid, txout: txout) if txid.present?
+  end
+
+  def find_wallet_as_reference
+    return blockchain.wallets.find_by_address(to_address) if to_wallet?
+    return blockchain.wallets.find_by_address(from_address) if from_wallet?
+  end
+
+  def update_reference
+    self.reference ||= find_reference
+    report_exception "Can't detect transaction reference #{id}", true, id: id, txid: txid if self.reference.nil? && success?
+  end
+end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   include TransactionKindDefiner
+  include TransactionReferences
 
   upsert_keys [:blockchain_id, :txid, :txout]
 
@@ -119,29 +120,5 @@ class Transaction < ApplicationRecord
 
   def transaction_url
     blockchain.explore_transaction_url txid if blockchain
-  end
-
-  private
-
-  def find_reference
-    find_withdraw_as_reference || find_deposit_as_reference || find_wallet_as_reference
-  end
-
-  def find_withdraw_as_reference
-    blockchain.withdraws.find_by_txid(txid) if txid.present?
-  end
-
-  def find_deposit_as_reference
-    blockchain.deposits.find_by(txid: txid, txout: txout) if txid.present?
-  end
-
-  def find_wallet_as_reference
-    return blockchain.wallets.find_by_address(to_address) if to_wallet?
-    return blockchain.wallets.find_by_address(from_address) if from_wallet?
-  end
-
-  def update_reference
-    self.reference ||= find_reference
-    report_exception "Can't detect transaction reference #{id}", true, id: id, txid: txid if self.reference.nil?
   end
 end
