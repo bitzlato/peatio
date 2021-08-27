5513572bf617e956f310b79778097627cf56a5eb
Danil Pismenny <danil@brandymint.ru>
2021-08-27 18:07:36 +0300

    Remove legacy code from migration

--- db/migrate/20210820101018_add_explorer_contract_address_to_blockchain.rb
+++ db/migrate/20210820101018_add_explorer_contract_address_to_blockchain.rb
@@ -1,8 +1,5 @@
 class AddExplorerContractAddressToBlockchain < ActiveRecord::Migration[5.2]
   def change
     add_column :blockchains, :explorer_contract_address, :string
-    Blockchain.find_each do |b|
-      b.update explorer_contract_address: 'https://etherscan.io/token/#{contract_address}' if b.key.include? 'eth'
-    end
   end
 end
