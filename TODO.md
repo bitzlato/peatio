3806f3f8acc7f79c884633eb3c748f0ec52d829d
Danil Pismenny <danil@brandymint.ru>
2021-08-28 17:51:58 +0300

    Update db/structure.sql

--- TODO.md
+++
@@ -1,5 +0,0 @@
-* [ ] Не учитывать пополнение газа на колешек депонирования
-* [ ] remove secrets, access_token and uri from wallet
-* [ ] disable update currency of wallet
-* [ ] Peatio::Transcation заменить на BlockchainTransaction entity
-* [ ] Support internal ETH transcations https://etherscan.io/tx/0x88d71c80ff633c5b39bca2b70d400a34dfe3ae5f1d6c19e4293386a656f64ab3
--- db/structure.sql
+++ db/structure.sql
@@ -844,9 +844,9 @@ CREATE TABLE public.payment_addresses (
     details_encrypted character varying(1024),
     member_id bigint,
     remote boolean DEFAULT false NOT NULL,
+    blockchain_id bigint NOT NULL,
     balances jsonb DEFAULT '{}'::jsonb,
-    balances_updated_at timestamp without time zone,
-    blockchain_id bigint NOT NULL
+    balances_updated_at timestamp without time zone
 );
 
 
@@ -1199,8 +1199,8 @@ CREATE TABLE public.wallets (
     kind integer NOT NULL,
     settings_encrypted character varying(1024),
     balance jsonb,
-    enable_invoice boolean DEFAULT false NOT NULL,
     plain_settings json,
+    enable_invoice boolean DEFAULT false NOT NULL,
     blockchain_id bigint NOT NULL,
     use_as_fee_source boolean DEFAULT false NOT NULL,
     balance_updated_at timestamp without time zone
@@ -2795,7 +2795,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210722125206'),
 ('20210727101029'),
 ('20210803084921'),
-('20210803134756'),
 ('20210806112457'),
 ('20210806112458'),
 ('20210806131828'),
