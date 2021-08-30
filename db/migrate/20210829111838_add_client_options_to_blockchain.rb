3961ebf47cd2f14b6c12c1a217f86620c0a4ea02
Danil Pismenny <danil@brandymint.ru>
2021-08-30 12:00:25 +0300

    Fix specs

--- db/migrate/20210829111838_add_client_options_to_blockchain.rb
+++ db/migrate/20210829111838_add_client_options_to_blockchain.rb
@@ -1,7 +1,7 @@
 class AddClientOptionsToBlockchain < ActiveRecord::Migration[5.2]
   def change
     add_column :blockchains, :client_options, :jsonb, null: false, default: {}
-    Blockchain.where(key: 'eth-mainnet').update_all client_options: { refuel_gas_factor: 1.1, base_gas_limit: 22_000, token_gas_limit: 110_000 }
-    Blockchain.where(key: 'bsc-mainnet').update_all client_options: { refuel_gas_factor: 1.1, base_gas_limit: 22_000, token_gas_limit: 220_000 }
+    # Blockchain.where(key: 'eth-mainnet').update_all client_options: { refuel_gas_factor: 1.1, base_gas_limit: 22_000, token_gas_limit: 110_000 }
+    # Blockchain.where(key: 'bsc-mainnet').update_all client_options: { refuel_gas_factor: 1.1, base_gas_limit: 22_000, token_gas_limit: 220_000 }
   end
 end
