102109a0e1f7af4c90e2cfb1a30bd171704823a2
Danil Pismenny <danil@brandymint.ru>
2021-09-03 10:49:27 +0300

    Use current block instead of syncing in heco

--- app/gateways/ethereum_gateway.rb
+++ app/gateways/ethereum_gateway.rb
@@ -177,6 +177,10 @@ class EthereumGateway < AbstractGateway
     )
   end
 
+  def current_syncing_block
+    client.json_rpc(:eth_syncing).fetch('currentBlock').to_i(16)
+  end
+
   private
 
   def build_client
--- app/gateways/heco_gateway.rb
+++ app/gateways/heco_gateway.rb
@@ -1,6 +1,2 @@
 class HecoGateway < EthereumGateway
-  # eth_blockNumber always returns 0 ;(
-  def latest_block_number
-    client.json_rpc(:eth_syncing).fetch('currentBlock').to_i(16)
-  end
 end
