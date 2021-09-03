74885f594f487849ed45d29faacce1637967939d
Danil Pismenny <danil@brandymint.ru>
2021-09-03 22:36:01 +0300

    Improve BlockNumber model

--- app/models/block_number.rb
+++ app/models/block_number.rb
@@ -7,4 +7,12 @@ class BlockNumber < ApplicationRecord
     self.status = self.status.to_s
   end
   validates :status, presence: true, inclusion: { in: STATUSES }
+
+  def reprocess!
+    blockchain.service.process_block number
+  end
+
+  def transactions
+    blockchain.transactions.where(block_number: number)
+  end
 end
