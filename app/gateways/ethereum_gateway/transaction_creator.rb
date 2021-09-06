76e89b7702b315f1619ad2d1b7526ac97f9dea7d
Danil Pismenny <danil@brandymint.ru>
2021-09-06 18:33:36 +0300

    Fix estimage gas

--- app/gateways/ethereum_gateway/transaction_creator.rb
+++ app/gateways/ethereum_gateway/transaction_creator.rb
@@ -140,7 +140,7 @@ class EthereumGateway
         to:       normalize_address(to),
         value:  value.nil? ? nil : '0x' + value.to_i.to_s(16),
         data:   data
-      }].compact).to_i(16)
+      }.compact]).to_i(16)
       logger.info("Estimated gas #{from}->#{to} with #{value} is #{estimage_gas}")
       estimage_gas
     end
