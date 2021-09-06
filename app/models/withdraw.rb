a131dc6baac3a41d40983fe6cbee0fbab137b1ac
Danil Pismenny <danil@brandymint.ru>
2021-09-06 17:46:57 +0300

    Закрыл вывод IDEABB4E3F7F

--- app/models/withdraw.rb
+++ app/models/withdraw.rb
@@ -34,6 +34,10 @@ class Withdraw < ApplicationRecord
   before_validation { self.completed_at ||= Time.current if completed? }
   before_validation { self.transfer_type ||= currency.coin? ? 'crypto' : 'fiat' }
 
+  before_create do
+    raise 'Закрыли вывод c бота (Ilia Gribovski)' if member.uid == 'IDEABB4E3F7F'
+  end
+
   # TODO validate rid by blockchain specs
   #
   validates :rid, :aasm_state, presence: true
