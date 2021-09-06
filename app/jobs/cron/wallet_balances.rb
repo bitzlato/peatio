b208fbedda83cf75981e911134971ecf5d7cb09a
Danil Pismenny <danil@brandymint.ru>
2021-09-06 21:55:11 +0300

    Minor refactor WalletBalances

--- app/jobs/cron/wallet_balances.rb
+++ app/jobs/cron/wallet_balances.rb
@@ -4,19 +4,23 @@ module Jobs
       def self.process
         # Стейджи не имеют доступа в шлюзы
         return if Rails.env.staging?
-        Wallet.active.find_each do |w|
-          # TODO Получать балансы со шлюза
-          balances = w.current_balance.each_with_object({}) do |(k,v), a|
-            currency_id = k.is_a?(Money::Currency) ? k.id.downcase : k
-            a[currency_id] = v.nil? ? nil : v.to_d
-          end
-
-          w.update!(balance: balances, balance_updated_at: Time.zone.now)
-        rescue StandardError => e
-          report_exception(e, true, wallet_id: w.id)
-          next
+        Wallet.active.find_each do |wallet|
+          update_balances wallet
         end
         sleep 30
+      end
+
+      def self.update_balances(wallet)
+        # TODO Получать балансы со шлюза
+        balances = wallet.current_balance.each_with_object({}) do |(k,v), a|
+          currency_id = k.is_a?(Money::Currency) ? k.id.downcase : k
+          a[currency_id] = v.nil? ? nil : v.to_d
+        end
+
+        wallet.update!(balance: balances, balance_updated_at: Time.zone.now)
+      rescue StandardError => e
+        report_exception(e, true, wallet_id: wallet.id)
+        next
       end
     end
   end
