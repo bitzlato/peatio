63607a0b1ed1aff38c9cf7c6ecd6875d2a9ce3f7
mailer21 <metelew@gmail.com>
2021-09-12 13:12:01 +0300

    market-sandbox added to capistrano

---
+++ config/deploy/sandbox.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+set :rails_env, :staging
+set :disallow_pushing, false
+set :application, -> { 'peatio-sandbox' }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+
+server '217.182.138.99',
+       user: fetch(:user),
+       port: '22',
+       roles: %w[app db daemons].freeze,
+       ssh_options: { forward_agent: true }
