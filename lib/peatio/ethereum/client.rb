b2608718d0da1b414cb20295a369a36b101d74a5
Danil Pismenny <danil@brandymint.ru>
2021-09-13 09:26:33 +0300

    m

--- lib/peatio/ethereum/client.rb
+++ lib/peatio/ethereum/client.rb
@@ -6,7 +6,7 @@ module Ethereum
 
     class ResponseError < Error
       attr_reader :data
-      def initialize(code, message, data)
+      def initialize(code, message, data = '')
         @data = data
         super "#{message} (#{code})"
       end
@@ -27,7 +27,7 @@ module Ethereum
       # "Accept: application/json\nContent-Type: application/json\nUser-Agent: Faraday v1.5.1\n\n{\"jsonrpc\":\"2.0\",\"id\":11,\"method\":\"eth_estimateGas\",\"params\":[{\"gasPrice\":\"0x1b6f2970f1\",\"from\":\"0x97d92440fce7096eb7d3859d790ccaae6adb6bee\",\"to\":\"0xdac17f958d2ee523a2206206994597c13d831ec7\",\"data\":\"0xa9059cbb0000000000000000000000008a988dc81b42be7a3ad343c4d4fa3eac3ead3dc40000000000000000000000000000000000000000000000000000000000000001\"}]}"
       # rcontent-length: 160\ncontent-type: application/json; charset=utf-8\ndate: Fri, 10 Sep 2021 17:42:21 GMT\n\n{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32015,\"message\":\"Transaction execution error.\",\"data\":\"Internal(\\\"Requires higher than upper limit of 300292660\\\")\"},\"id\":11}\n"
       if error['message'].include?('Transaction execution error') && error['data'].include?('Requires higher than upper limit')
-        raise NoEnoughtAmount.new(error['code'], error['data']), ''
+        raise NoEnoughtAmount.new(error['code'], error['data'])
       else
         raise ResponseError.new(error['code'], error['message'], error['data'])
       end
