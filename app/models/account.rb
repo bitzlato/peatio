18eabaaa4fe379825c7af34ade16574e1f4ba6f9
Danil Pismenny <danil@brandymint.ru>
2021-09-27 15:52:36 +0300

    [#151] Add Account#verify_locked!

--- app/models/account.rb
+++ app/models/account.rb
@@ -54,7 +54,30 @@ class Account < ApplicationRecord
 
   def calculated_locked
     deposits.where(is_locked: true).sum(:amount) +
-      withdraws.where(is_locked: true).sum(:amount)
+      withdraws.where(is_locked: true).sum(:amount) +
+      locked_in_orders
+  end
+
+  def locked_in_orders
+    OrderAsk.active.where(member_id: id).where(quote_unit: currency_id).sum(:locked) +
+      OrderBid.active.where(member_id: id).where(base_unit: currency_id).sum(:locked)
+  end
+
+  def verify_locks
+    account.locked == calculated_locked
+  end
+
+  def verify_locks!
+    Bugsnag.notify 'Отличается сумма заблокированная на счёте и расчётная' do |b|
+      b.secerity = :warn
+      b.meta_data = {
+        account_id: id,
+        locked: locked,
+        calculated_locked: calculated_locked,
+        locked_in_orders: locked_in_orders
+      }
+    end
+    true # Пока всегда отдаём true
   end
 
   def payment_address
