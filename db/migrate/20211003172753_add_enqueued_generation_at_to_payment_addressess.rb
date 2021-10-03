b8b1ebd9bbda1fbc11e00b130fdaf4946e0d8519
Danil Pismenny <danil@brandymint.ru>
2021-10-03 20:36:59 +0300

    rubocop

--- db/migrate/20211003172753_add_enqueued_generation_at_to_payment_addressess.rb
+++ db/migrate/20211003172753_add_enqueued_generation_at_to_payment_addressess.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEnqueuedGenerationAtToPaymentAddressess < ActiveRecord::Migration[5.2]
   def change
     add_column :payment_addresses, :enqueued_generation_at, :timestamp
