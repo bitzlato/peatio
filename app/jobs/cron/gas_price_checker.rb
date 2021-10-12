2a85078572a11c327ac031998b56ee63a4ca3bac
Aleksandr B. Ivanov <radanisk@gmail.com>
2021-10-12 11:14:01 +0300

    Add min and max thresholds for gas price

--- app/jobs/cron/gas_price_checker.rb
+++ app/jobs/cron/gas_price_checker.rb
@@ -12,13 +12,15 @@ module Jobs
           max_gas_price = Rails.configuration.blockchains.dig(blockchain.key, 'max_gas_price')
           next if !blockchain.gateway.is_a?(EthereumGateway) || max_gas_price.nil? || (blockchain.high_transaction_price_at.present? && blockchain.high_transaction_price_at > 5.minutes.ago)
 
+          min_threshold = max_gas_price * 0.98
+          max_threshold = max_gas_price * 1.02
           gas_price = EthereumGateway::AbstractCommand.new(blockchain.gateway.client).fetch_gas_price
-          if gas_price < max_gas_price && !blockchain.high_transaction_price_at.nil?
+          if gas_price < min_threshold && !blockchain.high_transaction_price_at.nil?
             blockchain.update!(high_transaction_price_at: nil)
-            Peatio::SlackNotifier.instance.ping "Включил выводы для #{blockchain.name}. Цена газа #{gas_price * 1e-9} Gwei ниже, чем #{max_gas_price * 1e-9} Gwei."
-          elsif gas_price > max_gas_price && blockchain.high_transaction_price_at.nil?
+            Peatio::SlackNotifier.instance.ping "Включил выводы для #{blockchain.name}. Цена газа #{gas_price * 1e-9} Gwei ниже, чем #{min_threshold * 1e-9} Gwei."
+          elsif gas_price > max_threshold && blockchain.high_transaction_price_at.nil?
             blockchain.update!(high_transaction_price_at: Time.current)
-            Peatio::SlackNotifier.instance.ping "Отключил выводы для #{blockchain.name}. Цена газа #{gas_price * 1e-9} Gwei выше, чем #{max_gas_price * 1e-9} Gwei."
+            Peatio::SlackNotifier.instance.ping "Отключил выводы для #{blockchain.name}. Цена газа #{gas_price * 1e-9} Gwei выше, чем #{max_threshold * 1e-9} Gwei."
           end
         end
 
