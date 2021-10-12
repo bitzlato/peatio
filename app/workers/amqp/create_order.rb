cead95dd236a247fcd11a292e682ae3c2166fc51
Danil Pismenny <danil@brandymint.ru>
2021-10-12 19:33:57 +0300

    Rescue create_order.rb on wrong payload data type

--- app/workers/amqp/create_order.rb
+++ app/workers/amqp/create_order.rb
@@ -6,7 +6,11 @@ module Workers
       def process(payload)
         payload.symbolize_keys!
         member_uid = payload.fetch(:member_uid)
-        order_data = payload.fetch(:data).symbolize_keys
+        order_data = payload.fetch(:data)
+
+        raise 'payload data must be a Hash' unless order_data.is_a? Hash
+
+        order_data.symbolize_keys!
 
         member = ::Member.find_by!(uid: member_uid)
         market = ::Market.active.find_spot_by_symbol(order_data[:market])
@@ -16,7 +20,7 @@ module Workers
           market: market,
           **order_data.slice(:side, :ord_type, :price, :volume, :uuid)
         )
-      rescue ActiveRecord::RecordNotFound, KeyError => e
+      rescue StandardError, ActiveRecord::RecordNotFound, KeyError => e
         report_exception(e, true, payload)
       end
     end
