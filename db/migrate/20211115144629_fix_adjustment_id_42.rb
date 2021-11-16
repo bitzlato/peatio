d59b1e7eef182519e437d80eaec5ea4f920c524a
Eugene Stepanov <antynitr@gmail.com>
2021-11-16 13:42:26 +0300

    Added two deposit by 0.008btc for ID505131DE7B

--- db/migrate/20211115144629_fix_adjustment_id_42.rb
+++ db/migrate/20211115144629_fix_adjustment_id_42.rb
@@ -4,14 +4,25 @@ class FixAdjustmentId42 < ActiveRecord::Migration[5.2]
   def change
     return unless Rails.env.production?
 
-    member = Member.find_by(uid: 'ID2C4D9F6CA3')
+    member = Member.find_by(uid: 'ID505131DE7B') # bitzlato:Jayjay
     currency = Currency.find_by(id: :btc)
 
     deposit = Deposit.create!(
       type: Deposit.name,
       member: member,
       currency: currency,
-      amount: 0.016
+      amount: 0.008,
+      invoice_id: '14222639:30969-2'
+    )
+    deposit.accept!
+    deposit.dispatch!
+
+    deposit = Deposit.create!(
+      type: Deposit.name,
+      member: member,
+      currency: currency,
+      amount: 0.008,
+      invoice_id: '14222639:30969-3'
     )
     deposit.accept!
     deposit.dispatch!
