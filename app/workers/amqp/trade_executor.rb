08c3c143d64037a585d683aefdb2b287e3f48600
Danil Pismenny <danil@brandymint.ru>
2021-11-16 19:07:51 +0300

    Remove unused code from TradeExecutor worker

--- app/workers/amqp/trade_executor.rb
+++ app/workers/amqp/trade_executor.rb
@@ -3,11 +3,6 @@
 module Workers
   module AMQP
     class TradeExecutor < Base
-      def initialize(market = nil)
-        @market = market # Setupt but not used
-        super()
-      end
-
       def process(payload)
         # TODO: compare payload market
         ::Matching::Executor.new(payload.symbolize_keys).process
