17d5ae8e6345ca06ff1d0abca0b97e149bb360c1
Danil Pismenny <danil@brandymint.ru>
2021-11-16 11:35:40 +0300

    More logging in deposit_intention worker

--- app/workers/amqp/deposit_intention.rb
+++ app/workers/amqp/deposit_intention.rb
@@ -9,21 +9,30 @@ module Workers
         deposit = Deposit.find_by_id(payload[:deposit_id])
         unless deposit
           Rails.logger.warn do
-            'Unable to make deposit.'\
-              "Deposit with id: #{payload[:deposit_id]} doesn't exist"
+            { message: 'Deposit intention does not exists',
+              deposit_id: payload[:deposit_id],
+              serivce: :deposit_intention }
           end
           return
         end
 
-        Rails.logger.info("Create invoice for #{deposit.id}")
+        Rails.logger.info do
+          { message: 'Create invoice', deposit_id: deposit.id, serivce: :deposit_intention }
+        end
 
         deposit.blockchain.gateway.create_invoice! deposit
 
         # Repeat again
       rescue Bitzlato::Client::WrongResponse => e
+        Rails.logger.error do
+          { message: e.messaage, deposit_id: payload[:deposit_id], service: :deposit_intention }
+        end
         report_exception(e)
         raise e
       rescue StandardError => e
+        Rails.logger.error do
+          { message: e.messaage, deposit_id: payload[:deposit_id], service: :deposit_intention }
+        end
         raise e if is_db_connection_error?(e)
 
         report_exception(e)
