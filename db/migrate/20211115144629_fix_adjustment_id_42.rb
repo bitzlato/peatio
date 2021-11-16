1cc924b67912352f198670b135268b2f08fe7c70
Eugene Stepanov <antynitr@gmail.com>
2021-11-16 11:58:59 +0300

    run migration only for production

--- db/migrate/20211115144629_fix_adjustment_id_42.rb
+++ db/migrate/20211115144629_fix_adjustment_id_42.rb
@@ -2,12 +2,10 @@
 
 class FixAdjustmentId42 < ActiveRecord::Migration[5.2]
   def change
-    return if Rails.env.test?
+    return unless Rails.env.production?
 
     member = Member.find_by(uid: 'ID2C4D9F6CA3')
     currency = Currency.find_by(id: :btc)
-
-    return if member.blank? || currency.blank?
 
     deposit = Deposit.create!(
       type: Deposit.name,
