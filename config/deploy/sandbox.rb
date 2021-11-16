84f6b14fcc30683cc10a0a919c7de8300d5e2ba5
Danil Pismenny <danil@brandymint.ru>
2021-11-16 17:13:58 +0300

    minor

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -6,7 +6,7 @@ set :application, -> { 'peatio-sandbox' }
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 set :markets, %w[eth_bnb]
 
-set :systemd_daemon_instances, -> { %i[bargainer_job blockchain collector currency_pricer gas_price_chechker k_line liabilities_compactor payment_address_balancer remove_invoiced_deposits stats_member_pnl ticker wallet_balances] }
+set :systemd_daemon_instances,  %i[bargainer_job blockchain collector currency_pricer gas_price_chechker k_line liabilities_compactor payment_address_balancer remove_invoiced_deposits stats_member_pnl ticker wallet_balances]
 
 server '217.182.138.99',
        user: fetch(:user),
