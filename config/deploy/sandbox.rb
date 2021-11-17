c637e5f3a5cc7e225e3ce2665568093e7ed7b9e6
mailer21 <metelew@gmail.com>
2021-11-17 20:21:38 +0300

    sandbox moved from ex5 to ex10

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -2,14 +2,14 @@
 
 set :rails_env, :sandbox
 set :disallow_pushing, false
-set :application, -> { 'peatio-sandbox' }
+set :application, -> { 'peatio' }
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 set :markets, %w[eth_bnb]
 
 set :systemd_daemon_instances,
     %i[bargainer_job blockchain_processor collector currency_pricer gas_price_checker k_line liabilities_compactor payment_address_balancer remove_invoiced_deposits stats_member_pnl ticker wallet_balances]
 
-server '217.182.138.99',
+server '141.94.218.39',
        user: fetch(:user),
        port: '22',
        roles: %w[app db daemons amqp_daemons market_amqp_daemons].freeze,
