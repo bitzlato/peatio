9f91f2f2001472776896a3407d4637a3beb6b1b9
Eugene Stepanov <antynitr@gmail.com>
2021-11-23 13:46:44 +0300

    Wrap create order and swap_order in transaction

--- app/services/order_services/create_swap_order.rb
+++ app/services/order_services/create_swap_order.rb
@@ -13,10 +13,16 @@ module OrderServices
         market: market, side: side, price: price, volume: volume, uuid: uuid
       }
 
-      swap_order = SwapOrder.create!(params.merge(member: @member, state: SwapOrder::STATES[:pending]))
-      create_order_result = CreateOrder.new(member: @member).perform(params.merge(ord_type: 'limit'))
+      swap_order = nil
+      create_order_result = nil
 
-      if create_order_result.data
+      ActiveRecord::Base.transaction do
+        swap_order = SwapOrder.create!(params.merge(member: @member, state: SwapOrder::STATES[:pending]))
+        create_order_result = CreateOrder.new(member: @member).perform(params.merge(ord_type: 'limit'))
+        raise ActiveRecord::Rollback if create_order_result.failed?
+      end
+
+      if create_order_result.successful?
         success(data: swap_order)
       else
         failure(errors: create_order_result.errors)
