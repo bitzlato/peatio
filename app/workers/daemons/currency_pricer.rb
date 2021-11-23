733b3521e31d849008c439cacacc550e42892ff9
Aleksandr B. Ivanov <radanisk@users.noreply.github.com>
2021-11-23 17:52:59 +0300

    Fix stagings (#275)

--- app/workers/daemons/currency_pricer.rb
+++ app/workers/daemons/currency_pricer.rb
@@ -6,7 +6,12 @@ module Workers
       @sleep_time = 5.minutes
 
       def process
-        quote_currency = Currency.find(Rails.configuration.currencies['currency_pricer']['quote_currency_code'])
+        quote_currency = Currency.find_by(id: Rails.configuration.currencies['currency_pricer']['quote_currency_code'])
+        if quote_currency.nil?
+          Rails.logger.warn { { message: 'Quote currency is not found', currency_id: Rails.configuration.currencies['currency_pricer']['quote_currency_code'], service: 'currency_pricer' } }
+          return
+        end
+
         Currency.find_each do |currency|
           price = CurrencyServices::Price.new(base_currency: currency, quote_currency: quote_currency).call
           next if price.nil?
