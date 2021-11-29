90f729078c8ab85e706e3d54f36610886a2e18be
Eugene Stepanov <antynitr@gmail.com>
2021-11-29 10:43:37 +0300

    Override vwap_time in currency pricer

--- app/services/currency_services/price.rb
+++ app/services/currency_services/price.rb
@@ -2,13 +2,16 @@
 
 module CurrencyServices
   class Price
-    def initialize(base_currency:, quote_currency:)
+    def initialize(base_currency:, quote_currency:, vwap_time: nil)
       @base_currency = base_currency
       @quote_currency = quote_currency
+      @vwap_time = vwap_time || Rails.configuration.currencies['currency_pricer']['vwap_time']
     end
 
-    def call
-      price = market_price(base_currency_code: @base_currency.code, quote_currency_code: @quote_currency.code)
+    def call(vwap_time: nil)
+      vwap_time = vwap_time || @vwap_time
+
+      price = market_price(base_currency_code: @base_currency.code, quote_currency_code: @quote_currency.code, vwap_time: vwap_time)
       if price.nil?
         intermediate_currency_codes = @quote_currency.dependent_markets.spot.pluck(:base_unit, :quote_unit).flatten.uniq - [@quote_currency.code]
         secondary_market = Market.where(base_unit: @base_currency.code, quote_unit: intermediate_currency_codes).or(
@@ -19,8 +22,8 @@ module CurrencyServices
           return nil
         end
         intermediate_currency_code = ([secondary_market.base_unit, secondary_market.quote_unit] - [@base_currency.code])[0]
-        primary_price = market_price(base_currency_code: intermediate_currency_code, quote_currency_code: @quote_currency.code)
-        secondary_price = market_price(base_currency_code: @base_currency.code, quote_currency_code: intermediate_currency_code)
+        primary_price = market_price(base_currency_code: intermediate_currency_code, quote_currency_code: @quote_currency.code, vwap_time: vwap_time)
+        secondary_price = market_price(base_currency_code: @base_currency.code, quote_currency_code: intermediate_currency_code, vwap_time: vwap_time)
         return nil if primary_price.nil? || secondary_price.nil?
 
         price = primary_price * secondary_price
@@ -30,7 +33,7 @@ module CurrencyServices
 
     private
 
-    def market_price(base_currency_code:, quote_currency_code:)
+    def market_price(base_currency_code:, quote_currency_code:, vwap_time:)
       is_forward_market = true
       market = Market.spot.find_by(base_unit: base_currency_code, quote_unit: quote_currency_code)
       if market.nil?
@@ -39,7 +42,7 @@ module CurrencyServices
       end
       return nil if market.nil?
 
-      price = market.vwap(Rails.configuration.currencies['currency_pricer']['vwap_time'])
+      price = market.vwap(vwap_time)
       return nil if price.nil?
 
       is_forward_market ? price : 1.0 / price
