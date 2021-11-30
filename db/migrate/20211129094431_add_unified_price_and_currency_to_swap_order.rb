6b5cbc834ec3f1597f810ad901a1c02f094f35a7
Eugene Stepanov <antynitr@gmail.com>
2021-11-30 12:35:18 +0300

    fix migtation

--- db/migrate/20211129094431_add_unified_price_and_currency_to_swap_order.rb
+++ db/migrate/20211129094431_add_unified_price_and_currency_to_swap_order.rb
@@ -7,14 +7,14 @@ class AddUnifiedPriceAndCurrencyToSwapOrder < ActiveRecord::Migration[5.2]
     add_column :swap_orders, :unified_unit, :string, index: true
 
     config = Rails.application.config_for(:swap)
-    vwap_time = config[:vwap_time]
-    unified_currency = Currency.find_by(config[:unified_currency_code])
+    vwap_time = config['vwap_time']
+    unified_currency = Currency.find_by(id: config['unified_currency_code'])
 
     SwapOrder.find_each do |swap_order|
-      price_service = CurrencyServices::Price.new(base_currency: swap_order.from_currency, quote_currency: swap_order.unified_currency, vwap_time: vwap_time)
-      unified_price = price_service.call
-      unified_total_amount = unified_price * volume
-      swap_order.update unified_total_amount: unified_total_amount, unified_price: unified_price, unified_currency: unified_currency
+      price_service = CurrencyServices::Price.new(base_currency: swap_order.from_currency, quote_currency: unified_currency, vwap_time: vwap_time)
+      unified_price = price_service.call.to_d
+      unified_total_amount = unified_price * swap_order.volume
+      swap_order.update unified_total_amount: unified_total_amount.to_d, unified_price: unified_price.to_d, unified_currency: unified_currency
     end
 
     change_column_null(:swap_orders, :unified_price, false)
