6f230acae65414201b04a9b6ed0b0bdaf34f2596
Eugene Stepanov <antynitr@gmail.com>
2021-12-01 12:55:14 +0300

    set sleep_time=10 for SwapOrderStatusChecker

--- app/workers/daemons/swap_order_status_checker.rb
+++ app/workers/daemons/swap_order_status_checker.rb
@@ -3,23 +3,29 @@
 module Workers
   module Daemons
     class SwapOrderStatusChecker < Base
-      @sleep_time = 15
+      @sleep_time = 10
 
       def process
-        SwapOrder.open.includes(:order).find_each do |swap_order|
-          order = swap_order.order
-          state = case order.state
-                  when 'cancel', 'reject'
-                    'cancel'
-                  when 'done'
-                    'done'
-                  when 'pending', 'wait'
-                    'wait'
-                  else
-                    raise "Unsupported order state: #{order.state}"
-                  end
+        swap_orders = SwapOrder.open.includes(:order)
 
+        swap_orders.find_each do |swap_order|
+          state = get_state_for!(swap_order)
           swap_order.update!(state: state)
+        end
+      end
+
+      private
+
+      def get_state_for!(swap_order)
+        case swap_order.order.state
+        when 'cancel', 'reject'
+          'cancel'
+        when 'done'
+          'done'
+        when 'pending', 'wait'
+          'wait'
+        else
+          raise "Unsupported order state: #{order.state}"
         end
       end
     end
