3393124a5e8ce1f43129216c2454faf21a75d13e
Eugene Stepanov <antynitr@gmail.com>
2021-12-03 00:52:23 +0300

    rework swap price service

--- app/services/currency_services/swap_price.rb
+++ app/services/currency_services/swap_price.rb
@@ -5,10 +5,15 @@ module CurrencyServices
     PRICE_DEVIATION = '0.02'.to_d
     PRICE_DEVIATION_PRECISION = 2
 
-    def initialize(from_currency:, to_currency:, request_volume:)
+    PriceObject = Struct.new(:from_currency, :to_currency, :request_currency,
+                             :request_volume, :request_price, :inverse_price,
+                             :from_volume, :to_volume)
+
+    def initialize(from_currency:, to_currency:, volume_currency:, volume:)
       @from_currency = from_currency
       @to_currency = to_currency
-      @request_volume = request_volume.to_d
+      @volume_currency = volume_currency
+      @volume = volume.to_d
     end
 
     def market
@@ -42,46 +47,98 @@ module CurrencyServices
 
     def price
       @price ||= begin
-        price = if sell?
-                  OrderBid.top_price(market.symbol).yield_self { |p| p - (p * PRICE_DEVIATION) }
-                else
-                  OrderAsk.top_price(market.symbol).yield_self { |p| p + (p * PRICE_DEVIATION) }
-                end
-
+        deviation = market_price * PRICE_DEVIATION
+        price = sell? ? market_price - deviation : market_price + deviation
         market.round_price(price.to_d)
       end
     end
 
     def inverse_price
-      market.round_price(1 / price)
-    end
-
-    def request_price
-      if sell?
-        price
-      else
-        inverse_price
-      end
-    end
-
-    def request_volume
-      market.round_amount(@request_volume)
-    end
-
-    def volume
-      if sell?
-        request_volume
-      else
-        market.round_amount(request_volume / price)
-      end
-    end
-
-    def inverse_volume
-      market.round_amount(request_volume * request_price)
+      (1 / price).to_d.round(8)
     end
 
     def valid_price?(price)
-      ((request_price - price) / price).abs.round(PRICE_DEVIATION_PRECISION) <= PRICE_DEVIATION
+      ((price_object.request_price - price) / price).abs.round(PRICE_DEVIATION_PRECISION) <= PRICE_DEVIATION
+    end
+
+    def price_object
+      @price_object ||= PriceObject.new.tap do |price_obj|
+        price_obj[:from_currency]     = @from_currency
+        price_obj[:to_currency]       = @to_currency
+        price_obj[:request_currency]  = @volume_currency
+        price_obj[:request_volume]    = @volume
+
+        if @volume_currency == market.base
+          price_obj[:request_price] = price
+          price_obj[:inverse_price] = inverse_price
+          if @volume_currency == @from_currency
+            price_obj[:from_volume] = @volume
+            price_obj[:to_volume] = market.round_amount(price * @volume)
+          elsif @volume_currency == @to_currency
+            price_obj[:from_volume] = market.round_amount(inverse_price * @volume)
+            price_obj[:to_volume] = @volume
+          end
+        elsif @volume_currency == market.quote
+          price_obj[:request_price] = inverse_price
+          price_obj[:inverse_price] = price
+          if @volume_currency == @from_currency
+            price_obj[:from_volume] = @volume
+            price_obj[:to_volume] = market.round_amount(inverse_price * @volume)
+          elsif @volume_currency == @to_currency
+            price_obj[:from_volume] = market.round_amount(price * @volume)
+            price_obj[:to_volume] = @volume
+          end
+        end
+      end
+    end
+
+
+    # Return prices and amounts
+    # automaticaly convert amount in quote to base
+    # [[price, amount]]
+    # [[0.7403e1, 0.989e0], [0.7403609e1, 0.98891864764]]
+    def raw_market_prices_with_amounts
+      return @raw_market_prices_with_amounts if @raw_market_prices_with_amounts
+
+      market_depth = sell? ? OrderBid.get_depth(market.symbol) : OrderAsk.get_depth(market.symbol)
+      volume = @volume.dup
+
+      @raw_market_prices_with_amounts = if market.base == @volume_currency
+        market_depth.each_with_object([]) do |(p, v), arr|
+          if volume <= v
+            arr << [p, volume]
+            volume = 0
+          else
+            arr << [p, v]
+            volume -= v
+          end
+          break arr if volume.zero?
+        end
+      elsif market.quote == @volume_currency
+        market_depth.each_with_object([]) do |(p, v), arr|
+          converted_volume = (1 / p) * volume
+          if converted_volume <= v
+            arr << [p, converted_volume]
+            volume = 0
+          else
+            arr << [p, v]
+            volume -= (converted_volume - v) * p
+          end
+          break arr if volume.zero?
+        end
+      else
+        raise "Volume currency must be a #{market.base_unit} or #{market.quote_unit}"
+      end
+    end
+
+    def market_amount
+      @market_amount ||= raw_market_prices_with_amounts.sum { |_price, volume| volume }
+    end
+
+    def market_price
+      @market_price ||= raw_market_prices_with_amounts.yield_self do |arr|
+                          arr.sum { |price, volume| price * volume } / market_amount
+                        end
     end
   end
 end
--- spec/services/currency_services/swap_price_spec.rb
+++ spec/services/currency_services/swap_price_spec.rb
@@ -5,58 +5,142 @@ RSpec.describe CurrencyServices::SwapPrice do
   let(:to_currency) { find_or_create(:currency, id: :usd) }
   let(:market) { Market.find_by(symbol: 'btc_usd') }
   let(:deviation) { 0.02 }
-  let(:top_price) { 10.to_d * market.price_precision }
+  let(:top_price) { 100.to_d * market.price_precision }
+
+  before do
+    OrderBid.stubs(:get_depth).returns([[top_price, 100.to_d]])
+    OrderAsk.stubs(:get_depth).returns([[top_price, 100.to_d]])
+  end
 
   context 'sale btc for usd' do
-    subject { described_class.new(from_currency: from_currency, to_currency: to_currency, request_volume: request_volume) }
-
     let(:request_volume) { 3 }
     let(:price) { market.round_price(top_price - (top_price * deviation)) }
-    let(:inverse_price) { market.round_price(1 / price) }
+    let(:inverse_price) { (1 / price).round(8) }
+    let(:volume) { market.round_amount(request_volume * price) }
 
-    before { OrderBid.stubs(:top_price).returns(top_price) }
+    context 'when volume_currency is from_currency' do
+      subject { described_class.new(from_currency: from_currency, to_currency: to_currency, volume_currency: from_currency, volume: request_volume) }
 
-    it { expect(subject.market).to eq(market) }
-    it { expect(subject.market?).to be(true) }
-    it { expect(subject.side).to eq('sell') }
-    it { expect(subject.sell?).to be(true) }
-    it { expect(subject.buy?).to be(false) }
-    it { expect(subject.price).to eq(price) }
-    it { expect(subject.inverse_price).to eq(inverse_price) }
-    it { expect(subject.request_price).to eq(price) }
-    it { expect(subject.request_volume).to eq(request_volume) }
-    it { expect(subject.volume).to eq(request_volume) }
+      it { expect(subject.market).to eq(market) }
+      it { expect(subject.market?).to be(true) }
+      it { expect(subject.side).to eq('sell') }
+      it { expect(subject.sell?).to be(true) }
+      it { expect(subject.buy?).to be(false) }
+      it { expect(subject.price).to eq(price) }
+      it { expect(subject.inverse_price).to eq(inverse_price) }
+      it do
+        p = price - (price * deviation)
+        expect(subject.valid_price?(p)).to eq(true)
+        expect(subject.valid_price?(p * 1.2)).to eq(false) # +20%
+      end
 
-    it do
-      p = price - (price * deviation)
-      expect(subject.valid_price?(p)).to eq(true)
+      context 'price object' do
+        let(:price_object) { subject.price_object }
+
+        it { expect(price_object.from_currency).to eq(from_currency) }
+        it { expect(price_object.to_currency).to eq(to_currency) }
+        it { expect(price_object.request_currency).to eq(from_currency) }
+        it { expect(price_object.request_volume).to eq(request_volume) }
+        it { expect(price_object.request_price ).to eq(price) }
+        it { expect(price_object.inverse_price ).to eq(inverse_price) }
+        it { expect(price_object.from_volume ).to eq(request_volume) }
+        it { expect(price_object.to_volume ).to eq(volume) }
+      end
+    end
+
+    context 'when volume_currency is to_currency' do
+      subject { described_class.new(from_currency: from_currency, to_currency: to_currency, volume_currency: to_currency, volume: request_volume) }
+
+      it { expect(subject.market).to eq(market) }
+      it { expect(subject.market?).to be(true) }
+      it { expect(subject.side).to eq('sell') }
+      it { expect(subject.sell?).to be(true) }
+      it { expect(subject.buy?).to be(false) }
+      it { expect(subject.price).to eq(price) }
+      it { expect(subject.inverse_price).to eq(inverse_price) }
+      it do
+        expect(subject.valid_price?(inverse_price)).to eq(true)
+        expect(subject.valid_price?(inverse_price * 1.2)).to eq(false) # +20%
+      end
+
+      context 'price object' do
+        let(:price_object) { subject.price_object }
+
+        it { expect(price_object.from_currency).to eq(from_currency) }
+        it { expect(price_object.to_currency).to eq(to_currency) }
+        it { expect(price_object.request_currency).to eq(to_currency) }
+        it { expect(price_object.request_volume).to eq(request_volume) }
+        it { expect(price_object.request_price ).to eq(inverse_price) }
+        it { expect(price_object.inverse_price ).to eq(price) }
+        it { expect(price_object.from_volume ).to eq(volume) }
+        it { expect(price_object.to_volume).to eq(request_volume) }
+      end
     end
   end
 
-  context 'buy btc for usd' do
-    subject { described_class.new(from_currency: to_currency, to_currency: from_currency, request_volume: request_volume) }
-
+  describe 'buy btc for usd' do
     let(:request_volume) { 20 }
     let(:price) { market.round_price(top_price + (top_price * deviation)) }
-    let(:inverse_price) { market.round_price(1 / price) }
-    let(:volume) { market.round_amount(request_volume / price) }
+    let(:inverse_price) {(1 / price).round(8) }
+    let(:volume) { market.round_amount(request_volume * inverse_price) }
 
-    before { OrderAsk.stubs(:top_price).returns(top_price) }
+    context 'when volume_currency is from_currency' do
+      subject { described_class.new(from_currency: to_currency, to_currency: from_currency, volume_currency: to_currency, volume: request_volume) }
 
-    it { expect(subject.market).to eq(market) }
-    it { expect(subject.market?).to be(true) }
-    it { expect(subject.side).to eq('buy') }
-    it { expect(subject.sell?).to be(false) }
-    it { expect(subject.buy?).to be(true) }
-    it { expect(subject.price).to eq(price) }
-    it { expect(subject.inverse_price).to eq(inverse_price) }
-    it { expect(subject.request_price).to eq(inverse_price) }
-    it { expect(subject.request_volume).to eq(request_volume) }
-    it { expect(subject.volume).to eq(volume) }
+      it { expect(subject.market).to eq(market) }
+      it { expect(subject.market?).to be(true) }
+      it { expect(subject.side).to eq('buy') }
+      it { expect(subject.sell?).to be(false) }
+      it { expect(subject.buy?).to be(true) }
+      it { expect(subject.price).to eq(price) }
+      it { expect(subject.inverse_price).to eq(inverse_price) }
+      it do
+        p = inverse_price + (inverse_price * deviation)
+        expect(subject.valid_price?(p)).to eq(true)
+        expect(subject.valid_price?(p * 0.8)).to eq(false) # -20%
+      end
 
-    it do
-      p = inverse_price + (inverse_price * deviation)
-      expect(subject.valid_price?(p)).to eq(true)
+      context 'price object' do
+        let(:price_object) { subject.price_object }
+
+        it { expect(price_object.from_currency).to eq(to_currency) }
+        it { expect(price_object.to_currency).to eq(from_currency) }
+        it { expect(price_object.request_currency).to eq(to_currency) }
+        it { expect(price_object.request_volume).to eq(request_volume) }
+        it { expect(price_object.request_price ).to eq(inverse_price) }
+        it { expect(price_object.inverse_price ).to eq(price) }
+        it { expect(price_object.from_volume ).to eq(request_volume) }
+        it { expect(price_object.to_volume ).to eq(volume) }
+      end
+    end
+
+    context 'when volume_currency is to_currency' do
+      subject { described_class.new(from_currency: to_currency, to_currency: from_currency, volume_currency: from_currency, volume: request_volume) }
+
+      it { expect(subject.market).to eq(market) }
+      it { expect(subject.market?).to be(true) }
+      it { expect(subject.side).to eq('buy') }
+      it { expect(subject.sell?).to be(false) }
+      it { expect(subject.buy?).to be(true) }
+      it { expect(subject.price).to eq(price) }
+      it { expect(subject.inverse_price).to eq(inverse_price) }
+      it do
+        expect(subject.valid_price?(price)).to eq(true)
+        expect(subject.valid_price?(price * 0.8)).to eq(false) # -20%
+      end
+
+      context 'price object' do
+        let(:price_object) { subject.price_object }
+
+        it { expect(price_object.from_currency).to eq(to_currency) }
+        it { expect(price_object.to_currency).to eq(from_currency) }
+        it { expect(price_object.request_currency).to eq(from_currency) }
+        it { expect(price_object.request_volume).to eq(request_volume) }
+        it { expect(price_object.request_price ).to eq(price) }
+        it { expect(price_object.inverse_price ).to eq(inverse_price) }
+        it { expect(price_object.from_volume ).to eq(volume) }
+        it { expect(price_object.to_volume ).to eq(request_volume) }
+      end
     end
   end
 end
