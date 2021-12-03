8c4bac85a6be3f62a7748fb552bf5b6a92833627
Eugene Stepanov <antynitr@gmail.com>
2021-12-03 10:11:02 +0300

    remove useless migrations

--- db/migrate/20211129094431_add_unified_price_and_currency_to_swap_order.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddUnifiedPriceAndCurrencyToSwapOrder < ActiveRecord::Migration[5.2]
-  def change
-    add_column :swap_orders, :unified_price, :decimal, precision: 32, scale: 16
-    add_column :swap_orders, :unified_total_amount, :decimal, precision: 32, scale: 16
-    add_column :swap_orders, :unified_unit, :string, index: true
-  end
-end
--- db/migrate/20211130120940_remove_unified_currency.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveUnifiedCurrency < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :swap_orders, :unified_unit
-    remove_column :swap_orders, :unified_total_amount
-    remove_column :swap_orders, :unified_price
-  end
-end
