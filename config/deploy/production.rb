f74db018f10c41e58f7945250d840060d354d17e
Eugene. S <evgenii.s@bitzlato.com>
2021-12-22 14:22:25 +0300

    update production market and bargainers (#325)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,17 @@ set :puma_bind, %w[tcp://0.0.0.0:9200]
 set :puma_workers, 4
 set :puma_threads, [4, 5]
 
-set :markets, %w[btc_usdterc20 btc_mcrerc20 eth_btc eth_usdterc20 eth_usdcerc20 eth_mcrerc20 usdterc20_mcrerc20 usdterc20_usdtbep20 usdterc20_usdcerc20 usdcerc20_usdcbep20 bnbbep20_usdtbep20 bnbbep20_usdcbep20 usdthrc20_usdtbep20 usdthrc20_usdterc20 usdchrc20_usdcerc20 usdchrc20_usdcbep20 hthrc20_usdthrc20 hthrc20_usdchrc20 mdterc20_mcrerc20 mdterc20_usdterc20 matic_usdtplgn matic_usdcplgn]
+set :markets, %w[
+  btc_usdterc20 btc_mcrerc20 eth_btc
+  eth_usdterc20 eth_usdcerc20 eth_mcrerc20
+  usdterc20_mcrerc20 usdterc20_usdtbep20 usdterc20_usdcerc20
+  usdcerc20_usdcbep20 bnbbep20_usdtbep20 bnbbep20_usdcbep20
+  usdthrc20_usdtbep20 usdthrc20_usdterc20 usdchrc20_usdcerc20
+  usdchrc20_usdcbep20 hthrc20_usdthrc20 hthrc20_usdchrc20
+  mdterc20_mcrerc20 mdterc20_usdterc20 matic_usdtplgn matic_usdcplgn
+  usdterc20_usdtplgn usdcerc20_usdcplgn usdtbep20_usdtplgn
+  usdcbep20_usdcplgn usdthrc20_usdtplgn usdchrc20_usdcplgn
+]
 
 server ENV['PRODUCTION_SERVER'],
        user: fetch(:user),
