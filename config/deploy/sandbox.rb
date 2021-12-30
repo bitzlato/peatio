abcadffd70b0f6c685c73d0dd217b53655c2ed07
mailer21 <metelew@gmail.com>
2021-12-30 19:20:50 +0300

    cleaning

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -9,7 +9,7 @@ set :markets, %w[eth_bnb]
 set :systemd_daemon_instances,
     %i[bargainer_job blockchain_processor collector currency_pricer gas_price_checker k_line liabilities_compactor payment_address_balancer remove_invoiced_deposits stats_member_pnl ticker wallet_balances swap_order_status_checker]
 
-server '141.94.218.39',
+server ENV.fetch('SANDBOX_SERVER'),
        user: fetch(:user),
        port: '22',
        roles: %w[app db daemons amqp_daemons market_amqp_daemons].freeze,
