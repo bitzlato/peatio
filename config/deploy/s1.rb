817ac1b75b94030cd70a3f18f7a74a5483b3b388
Danil Pismenny <danil@brandymint.ru>
2022-01-20 10:54:39 +0300

    Add multi bind for puma on stages

--- config/deploy/s1.rb
+++ config/deploy/s1.rb
@@ -5,6 +5,8 @@ set :disallow_pushing, false
 set :application, -> { 'peatio-' + fetch(:stage).to_s }
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
 
+set :puma_bind, -> { ["tcp://0.0.0.0:965#{fetch(:stage)[1].to_i || 9}", "unix://#{shared_path}/tmp/sockets/puma.sock"] }
+
 set :systemd_daemon_instances,
     %i[
       blockchain_processor
