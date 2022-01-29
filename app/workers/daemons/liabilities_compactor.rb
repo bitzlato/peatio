81bc292fa55f31e4dd6d3acf4e770081653251c3
Aleksandr B. Ivanov <radanisk@gmail.com>
2022-01-29 17:52:35 +0300

    Run liabilities compactor only at night

--- app/workers/daemons/liabilities_compactor.rb
+++ app/workers/daemons/liabilities_compactor.rb
@@ -7,10 +7,14 @@ Peatio::Application.load_tasks
 module Workers
   module Daemons
     class LiabilitiesCompactor < Base
-      @sleep_time = 24.hours
+      @sleep_time = 1.hour
 
       def process
+        return unless Time.current.hour == 1
+
+        Rails.logger.info { { message: 'Start liabilities compactor' } }
         Rake::Task['job:liabilities:compact_orders'].execute
+        Rails.logger.info { { message: 'Finish liabilities compactor' } }
       end
     end
   end
