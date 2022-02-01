8f09ad65124f51bf4cc6010dca74d9fe35c022f3
Aleksandr B. Ivanov <radanisk@gmail.com>
2022-02-01 15:12:34 +0300

    Fix compact_orders SQL-function
    Fixes: https://github.com/bitzlato/peatio/issues/355

--- db/structure.sql
+++ db/structure.sql
@@ -60,7 +60,7 @@ CREATE FUNCTION public.compact_orders(min_date date, max_date date, OUT pointer 
     AS $$
     BEGIN
         -- Temporary liabilities table
-        CREATE TABLE IF NOT EXISTS liabilities_tmp AS TABLE liabilities;
+        CREATE TEMPORARY TABLE IF NOT EXISTS liabilities_tmp(LIKE liabilities INCLUDING ALL);
 
         -- Copy liabilities to tmp
         INSERT INTO liabilities_tmp SELECT * FROM liabilities
@@ -68,20 +68,17 @@ CREATE FUNCTION public.compact_orders(min_date date, max_date date, OUT pointer 
 
         -- Set counter and pointer vars
         get diagnostics counter = row_count;
-        SELECT to_char(max_date, 'YYYYMMDD')::integer from liabilities INTO pointer;
+        SELECT to_char(max_date, 'YYYYMMDD')::integer INTO pointer;
 
         -- Delete liabilities to compact
         DELETE FROM liabilities WHERE LOWER(reference_type) = LOWER('Order') AND created_at BETWEEN min_date AND max_date;
 
-        CREATE SEQUENCE liabilities_tmp_id START 1 INCREMENT 1 MINVALUE 1 OWNED BY liabilities_tmp.id;
-
         INSERT INTO liabilities
-        SELECT nextval('liabilities_tmp_id') + (select max(id) + 1 from liabilities), code, currency_id, member_id, 'CompactOrders',
+        SELECT MAX(id), code, currency_id, member_id, 'CompactOrders',
         to_char(max_date, 'YYYYMMDD')::integer, SUM(debit)::decimal, SUM(credit)::decimal, DATE(created_at), NOW()::date FROM liabilities_tmp
         WHERE LOWER(reference_type) = LOWER('Order') AND created_at BETWEEN min_date AND max_date
         GROUP BY code, currency_id, member_id, DATE(created_at);
 
-        DROP SEQUENCE IF EXISTS liabilities_tmp_id;
         DROP TABLE liabilities_tmp;
     END
     $$;
@@ -3359,6 +3356,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20211226123302'),
 ('20220110182834'),
 ('20220113150944'),
-('20220113155904');
+('20220113155904'),
+('20220131124813');
 
 
