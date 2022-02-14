56adcb2e67a0e7e9790595f0864184f7e8543699
Aleksandr B. Ivanov <radanisk@gmail.com>
2022-02-14 11:57:15 +0300

    Update market jobs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,17 +8,8 @@ set :puma_workers, 4
 set :puma_threads, [4, 5]
 
 set :markets, %w[
-  btc_usdterc20 btc_mcrerc20 eth_btc
-  eth_usdterc20 eth_usdcerc20 eth_mcrerc20
-  usdterc20_mcrerc20 usdterc20_usdtbep20 usdterc20_usdcerc20
-  usdcerc20_usdcbep20 bnbbep20_usdtbep20 bnbbep20_usdcbep20
-  usdthrc20_usdtbep20 usdthrc20_usdterc20 usdchrc20_usdcerc20
-  usdchrc20_usdcbep20 hthrc20_usdthrc20 hthrc20_usdchrc20
-  mdterc20_mcrerc20 mdterc20_usdterc20 matic_usdtplgn matic_usdcplgn
-  usdterc20_usdtplgn usdcerc20_usdcplgn usdtbep20_usdtplgn
-  usdcbep20_usdcplgn usdthrc20_usdtplgn usdchrc20_usdcplgn
-  avax_usdtearc20 avax_usdcearc20 usdterc20_usdtearc20 usdcerc20_usdcearc20
-  usdtbep20_usdtearc20 usdcbep20_usdcearc20 usdthrc20_usdtearc20 usdchrc20_usdcearc20
+  btc_usdt btc_mcrerc20 eth_btc eth_usdt usdt_mcrerc20
+  bnbbep20_usdt hthrc20_usdt mdterc20_mcrerc20 matic_usdt avax_usdt
 ]
 
 server ENV['PRODUCTION_SERVER'],
