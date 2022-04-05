7c5a2ca03c6db900cf09589426f2366be569d055
Aleksandr B. Ivanov <radanisk@gmail.com>
2022-04-05 11:54:50 +0300

    Restore valid_address? for BitzlatoGateway

--- app/gateways/bitzlato_gateway.rb
+++ app/gateways/bitzlato_gateway.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class BitzlatoGateway < AbstractGateway
+  def self.valid_address?(address)
+    return false if BitcoinGateway.valid_address?(address)
+
+    # Take regexp from p2p, but increase length size
+    # https://chat.lgk.one/bitzlato/pl/e4ixc1wyxpy13d84tezdcbfy4a
+    address =~ /^[a-z][a-z0-9_]{1,27}$/i
+  end
+
   def build_client; end
 end
