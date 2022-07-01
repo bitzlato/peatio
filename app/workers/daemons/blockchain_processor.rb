ce8727d2cc6df2361ffb4bd3dbfad728ef54024c
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-07-01 17:05:41 +0300

    Disable blockchain processor for eth

--- app/workers/daemons/blockchain_processor.rb
+++ app/workers/daemons/blockchain_processor.rb
@@ -74,7 +74,7 @@ module Workers
       end
 
       def run
-        blockchains = ::Blockchain.active.where.not(key: %w[polygon-mainnet bitzlato-mainnet heco-mainnet avax-mainnet])
+        blockchains = ::Blockchain.active.where.not(key: %w[polygon-mainnet bitzlato-mainnet heco-mainnet avax-mainnet eth-mainnet])
         @runner_pool = blockchains.each_with_object({}) do |b, pool|
           max_timestamp = [b.currencies.maximum(:updated_at), b.updated_at].compact.max.to_i
 
