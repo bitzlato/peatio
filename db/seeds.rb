e83ee0e4d45f90729702f8a01a9c773d58d3f682
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-12-30 15:34:28 +0300

    [#184118608] Update seed for Ethereum testnet

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Rake::Task['seed:trading_fees'].invoke
 blockchain = Blockchain.find_by!(key: 'bitzlato')
 BlockchainCurrency.create!(blockchain: blockchain, currency: Currency.find('usd'), base_factor: 100)
 BlockchainCurrency.create!(blockchain: blockchain, currency: Currency.find('btc'), base_factor: 100_000_000)
-blockchain = Blockchain.find_by!(key: 'eth-ropsten')
+blockchain = Blockchain.find_by!(key: 'eth-testnet')
 parent_blockchain_currency = BlockchainCurrency.create!(blockchain: blockchain, currency: Currency.find('eth'), base_factor: 1_000_000_000_000_000_000, gas_limit: 21_000)
 BlockchainCurrency.create!(blockchain: blockchain, currency: Currency.find('trst'), contract_address: '0x0000000000000000000000000000000000000000', base_factor: 1_000_000, gas_limit: 90_000, parent_id: parent_blockchain_currency.id)
 blockchain = Blockchain.find_by!(key: 'bsc-testnet')
